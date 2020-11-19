e5be2723f14cd7452c5ad45fdd908b0d9b760b67
TanakitSuparp <73685800+TanakitSuparp@users.noreply.github.com>
2020-11-19 20:39:04 +0700

    Add files via upload

---
+++ insert1.php
@@ -0,0 +1,27 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'lab13-069.mysql.database.azure.com', 'it63070069@lab13-069', 'SMCbvq72', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+
+
+$sql = DELETE FROM geustbook;
+
+
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
